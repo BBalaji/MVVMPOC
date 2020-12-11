90c3a138363ccea20c0330d2904820c4414e54f6
BABESTA <capgemini.besta@daimler.com>
2020-12-11 14:40:02 +0530

    Removed Object
    Empty object test in UI

--- BalajiMVVMPoc/Service/Service.swift
+++ BalajiMVVMPoc/Service/Service.swift
@@ -17,7 +17,7 @@ struct Service {
 let infoMvvmData = [
     MVVMContactModel(image: "apple", title: "Kelly Goodwin", description: "Designer Designer Designer1 Designer2 Designer3 Designer4 Designer5 Designer Designer Designer Designer Designer Designer1 Designer2 Designer3 Designer4 Designer5 Designer "),
     MVVMContactModel(image: "apple", title: "Mohammad Hussain", description: "SEO Specialist"),
-    MVVMContactModel(image: "apple", title: "John Young", description: "Interactive Designer"),
+    MVVMContactModel(image: "", title: "", description: ""),
     MVVMContactModel(image: "apple", title: "Tamilarasi Mohan", description: "Architect"),
     MVVMContactModel(image: "apple", title: "Kim Yu", description: "Economist"),
     MVVMContactModel(image: "apple", title: "Shreya Nithin", description: "Web Strategist"),
