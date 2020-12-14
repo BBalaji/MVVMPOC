e0160e24264792e17476ac6fca499e43db5e674c
BABESTA <capgemini.besta@daimler.com>
2020-12-14 11:37:33 +0530

    UITableView Pull to Refresh
    UITableView Pull to Refresh Update Server API data

--- BalajiMVVMPoc/MVVM/View/Controller/MVVMTableViewController.swift
+++ BalajiMVVMPoc/MVVM/View/Controller/MVVMTableViewController.swift
@@ -14,7 +14,7 @@ class MVVMTableViewController: UITableViewController {
     var infoModelData : InfoModel?
     var rowModelData : [RowModel] = []
     var contactData : [MVVMContactModel] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "MVVM"
@@ -26,8 +26,18 @@ class MVVMTableViewController: UITableViewController {
         tableView.register(MVVMInfoTableViewCell.self, forCellReuseIdentifier: MVVMInfoTableViewCell.identifier)
 
         fetchServerInfoData()
+        //Pull to refresh UITableview data
+        let refreshControl = UIRefreshControl()
+            refreshControl.addTarget(self, action:  #selector(handleRefresh), for: .valueChanged)
+            self.refreshControl = refreshControl
+        refreshControl.tintColor = UIColor(red:0.25, green:0.72, blue:0.85, alpha:1.0)
+        refreshControl.attributedTitle = NSAttributedString(string: "Fetching Server Data ...")
     }
-    
+    //MARK:- Handle Refresh
+    @objc func handleRefresh() {
+        self.rowModelData.removeAll()
+        fetchServerInfoData()
+    }
     //MARK:- GCD Implemented
     func fetchServerInfoData()  {
         DispatchQueue.global(qos: .utility).async {
@@ -37,10 +47,12 @@ class MVVMTableViewController: UITableViewController {
                 case let .success(data):
                     self.title = data?.title
                     self.infoModelData = data
-                    self.rowModelData = data!.rows
-                    //Validate struct empty Variable
-                    self.rowModelData = self.rowModelData.filter { $0.title != nil && $0.rowDescription != "" && $0.imageHref != "" }
-                 self.tableView.reloadData()
+                    let tempRowModelData: [RowModel]? = data?.rows
+                    //Struct empty variable validation
+                    self.rowModelData = (tempRowModelData?.filter { $0.title != nil && $0.rowDescription != "" && $0.imageHref != "" })!
+//                    self.do_table_refresh()
+                    self.tableView.reloadData()
+                    self.refreshControl?.endRefreshing()
                  case let .failure(error):
                     print(error)
                  }
